5cb1146fa306cffd1fd752176c4044bf7120b76e
patschue <84413011+patschue@users.noreply.github.com>
2021-12-02 08:52:15 +0100

    Uncomment first part

--- Day2.py
+++ Day2.py
@@ -5,21 +5,21 @@ Created on Thu Dec  2 07:25:33 2021
 @author: schue
 """
 
-# with open("InputTag2.txt") as f:
-#     cmd = f.readline().split()
-#     horizontal = 0
-#     vertical = 0
-#     while cmd:
-#         if cmd[0] == "forward":
-#             horizontal += int(cmd[1])
-#         elif cmd[0] == "down":
-#             vertical += int(cmd[1])
-#         else:
-#             vertical -= int(cmd[1])
-#         cmd = f.readline().split()
-#     print(horizontal, vertical, horizontal * vertical)
+with open("InputDay2.txt") as f:
+    cmd = f.readline().split()
+    horizontal = 0
+    vertical = 0
+    while cmd:
+        if cmd[0] == "forward":
+            horizontal += int(cmd[1])
+        elif cmd[0] == "down":
+            vertical += int(cmd[1])
+        else:
+            vertical -= int(cmd[1])
+        cmd = f.readline().split()
+    print(horizontal, vertical, horizontal * vertical)
 
-with open("InputTag2.txt") as f:
+with open("InputDay2.txt") as f:
     cmd = f.readline().split()
     horizontal = 0
     vertical = 0
