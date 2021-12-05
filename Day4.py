6374d66a4b0df4bd9ea17947bb26995fc99a1a5b
patschue <84413011+patschue@users.noreply.github.com>
2021-12-05 14:23:40 +0100

    Update Day 4

--- Day4.py
+++ Day4.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  4 08:18:56 2021
+@author: Patrick
 
-@author: schue
+Don't touch, this code has no idea what it's supposed to do...
 """
 
 # with open("InputDay4.txt") as f:
@@ -38,122 +38,129 @@ secondBoard = [["x","x","x","x","x"],
 #                     secondBoards[board][row][column] = int(Zahl)
 #                     print(Zahl)
 
-def main():
-    with open("InputDay4.txt") as input:
-        print(part1(input))
-    with open('InputDay4.txt') as input:
-        print(part2(input))
-
-def part1(input):
-    randNums = []
-    boards = []
-    boardNum = 0
-    boardData = {}
-    boardLength = None
-
-    for line in input:
-        if len(randNums) == 0:
-            for num in line.strip().split(','):
-                randNums.append(int(num))
-        else:
-            if len(boards) == 0 and len(line.strip()) == 0:
-                continue
-            if len(line.strip()) == 0:
-                boardNum += 1
-                continue
-            if boardLength is None:
-                boardLength = len([i for i in line.split()])
-
-            if boardNum == len(boards):
-                boards.append([])
-
-            boards[boardNum].append([[int(num), 0] for num in line.split()])
-
-    bingo = False
-    bingoSum = 0
-    for num in randNums:
-        for board in boards:
-            board = markNum(board, num)
-            if checkBoard(board):
-                bingo = True
-                for row in board:
-                    for x in row:
-                        bingoSum += x[0] if x[1]==0 else 0
-                return bingoSum*num
 
 
-def markNum(board, n):
-    for row in board:
-        for num in row:
-            if num[0] == n:
-                num[1] = 1
+####################
+# Ideas from reddit
+####################
 
-    return board
 
-def checkBoard(board):
-    for row in board:
-        sum = 0
-        for num in row:
-            sum += num[1]
-        if sum == len(row):
-            return True
+# def main():
+#     with open("InputDay4.txt") as input:
+#         print(part1(input))
+#     with open('InputDay4.txt') as input:
+#         print(part2(input))
+
+# def part1(input):
+#     randNums = []
+#     boards = []
+#     boardNum = 0
+#     boardData = {}
+#     boardLength = None
+
+#     for line in input:
+#         if len(randNums) == 0:
+#             for num in line.strip().split(','):
+#                 randNums.append(int(num))
+#         else:
+#             if len(boards) == 0 and len(line.strip()) == 0:
+#                 continue
+#             if len(line.strip()) == 0:
+#                 boardNum += 1
+#                 continue
+#             if boardLength is None:
+#                 boardLength = len([i for i in line.split()])
+
+#             if boardNum == len(boards):
+#                 boards.append([])
+
+#             boards[boardNum].append([[int(num), 0] for num in line.split()])
+
+#     bingo = False
+#     bingoSum = 0
+#     for num in randNums:
+#         for board in boards:
+#             board = markNum(board, num)
+#             if checkBoard(board):
+#                 bingo = True
+#                 for row in board:
+#                     for x in row:
+#                         bingoSum += x[0] if x[1]==0 else 0
+#                 return bingoSum*num
+
+
+# def markNum(board, n):
+#     for row in board:
+#         for num in row:
+#             if num[0] == n:
+#                 num[1] = 1
+
+#     return board
+
+# def checkBoard(board):
+#     for row in board:
+#         sum = 0
+#         for num in row:
+#             sum += num[1]
+#         if sum == len(row):
+#             return True
     
-    for i in range(len(board[0])):
-        sum = 0
-        for row in board:
-            sum += row[i][1]
-        if sum == len(board[0]):
-            return True
+#     for i in range(len(board[0])):
+#         sum = 0
+#         for row in board:
+#             sum += row[i][1]
+#         if sum == len(board[0]):
+#             return True
 
-def part2(input):
-    randNums = []
-    boards = []
-    boardNum = 0
-    boardData = {}
-    boardLength = None
+# def part2(input):
+#     randNums = []
+#     boards = []
+#     boardNum = 0
+#     boardData = {}
+#     boardLength = None
 
-    for line in input:
-        if len(randNums) == 0:
-            for num in line.strip().split(','):
-                randNums.append(int(num))
-        else:
-            if len(boards) == 0 and len(line.strip()) == 0:
-                continue
-            if len(line.strip()) == 0:
-                boardNum += 1
-                continue
-            if boardLength is None:
-                boardLength = len([i for i in line.split()])
+#     for line in input:
+#         if len(randNums) == 0:
+#             for num in line.strip().split(','):
+#                 randNums.append(int(num))
+#         else:
+#             if len(boards) == 0 and len(line.strip()) == 0:
+#                 continue
+#             if len(line.strip()) == 0:
+#                 boardNum += 1
+#                 continue
+#             if boardLength is None:
+#                 boardLength = len([i for i in line.split()])
 
-            if boardNum == len(boards):
-                boards.append([])
+#             if boardNum == len(boards):
+#                 boards.append([])
 
-            boards[boardNum].append([[int(num), 0] for num in line.split()])
-
-
-    bingo = False
-    bingoNum = 0
-    bingoSum = 0
-    for num in randNums:
-        for i in range(len(boards)-1, -1, -1):
-            board = boards[i]
-            board = markNum(board, num)
-            if checkBoard(board):
-                if len(boards) != 1:
-                    boards.remove(board)
-                else:
-                    bingoNum = num
-                    bingo = True
-                    break
-        if bingo:
-            break
-
-    sum =0
-    for row in boards[0]:
-        for num in row:
-            bingoSum += num[0] if num[1]==0 else 0
-    return bingoSum * bingoNum
+#             boards[boardNum].append([[int(num), 0] for num in line.split()])
 
 
-if __name__=='__main__':
-    main()
+#     bingo = False
+#     bingoNum = 0
+#     bingoSum = 0
+#     for num in randNums:
+#         for i in range(len(boards)-1, -1, -1):
+#             board = boards[i]
+#             board = markNum(board, num)
+#             if checkBoard(board):
+#                 if len(boards) != 1:
+#                     boards.remove(board)
+#                 else:
+#                     bingoNum = num
+#                     bingo = True
+#                     break
+#         if bingo:
+#             break
+
+#     sum =0
+#     for row in boards[0]:
+#         for num in row:
+#             bingoSum += num[0] if num[1]==0 else 0
+#     return bingoSum * bingoNum
+
+
+# if __name__=='__main__':
+#     main()
